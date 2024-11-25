6a148415c5afbce57666a886fff584cd8f663a97
DuongDuc15 <duongduc.ctb@gmail.com>
2024-11-26 06:55:48 +0700

    Update code

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -18,11 +18,19 @@ provider "aws" {
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
 resource "aws_instance" "Udacity_T2" {
   count = "4"
-  ami = "ami-ad4812c8"
+  ami = "ami-09e4ba81d75ebeb6a"
   instance_type = "t2.micro"
   tags = {
     Name = "Udacity T2"
   }
 }
 
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
+# # TODO: provision 2 m4.large EC2 instances named Udacity M4
+# resource "aws_instance" "Udacity_M4" {
+#   count = "2"
+#   ami = "ami-09e4ba81d75ebeb6a"
+#   instance_type = "m4.large"
+#   tags = {
+#    Name = "Udacity M4"
+#  }
+# }
